1c4986f7d77b26b8d1b6ea3c0fe3ad02fb950a37
snwasiswa <ste01was@outlook.com>
2020-06-22 11:05:52 -0500

    The product is in development

--- textsummary.py
+++ textsummary.py
@@ -14,7 +14,6 @@ class Summary:
         self.all_sentences = sent_tokenize(self.text)
         self.score_sentence = {}
 
-        #self.words = words
 
     # Function that removes stop words
     def tokenize_text(self):
@@ -41,8 +40,6 @@ class Summary:
 
     def get_summary(self):
 
-        #score_sentence = {}
-
         for sentence in self.all_sentences:
              for word, score in self.score_word().items():
                  if word in sentence.lower():
@@ -68,18 +65,18 @@ class Summary:
             if (sentence in self.score_sentence) and (self.score_sentence[sentence] > (1.1 * average_value)):
                 summary += " " + sentence
 
-        return summary
+        print(summary)
 
 
-def main():
+#def main():
 
-    sum = Summary("There are many techniques available to generate extractive summarization to keep it simple, I will be using an unsupervised learning approach to find the sentences similarity and rank them. Summarization can be defined as a task of producing a concise and fluent summary while preserving key information and overall meaning. One benefit of this will be, you don’t need to train and build a model prior start using it for your project. It’s good to understand Cosine similarity to make the best use of the code you are going to see. Cosine similarity is a measure of similarity between two non-zero vectors of an inner product space that measures the cosine of the angle between them. Its measures cosine of the angle between vectors. The angle will be 0 if sentences are similar.")
+    #sum = Summary("There are many techniques available to generate extractive summarization to keep it simple, I will be using an unsupervised learning approach to find the sentences similarity and rank them. Summarization can be defined as a task of producing a concise and fluent summary while preserving key information and overall meaning. One benefit of this will be, you don’t need to train and build a model prior start using it for your project. It’s good to understand Cosine similarity to make the best use of the code you are going to see. Cosine similarity is a measure of similarity between two non-zero vectors of an inner product space that measures the cosine of the angle between them. Its measures cosine of the angle between vectors. The angle will be 0 if sentences are similar.")
 
-    print(sum.get_summary())
+    #sum.get_summary()
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-    main()
+    #main()
 
 
 
